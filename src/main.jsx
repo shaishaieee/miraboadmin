ff6236f1e6d63a0bfe65c8353056fd54a3589ebe
Hernandez, Reuvin <127575733+reuvin22@users.noreply.github.com>
2025-02-27 18:23:20 +0800

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import "react-toastify/dist/ReactToastify.css";
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>,
+  </BrowserRouter>,
 )
