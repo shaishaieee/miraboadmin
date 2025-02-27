7e16b8ad0fede9a1453f6877164a56b8b83027a9
Hernandez, Reuvin <127575733+reuvin22@users.noreply.github.com>
2025-02-27 18:22:10 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,45 @@
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Login from './components/Login';
-import ForgetPassword from './components/ForgetPassword';
-import Dashboard from './components/Dashboard';
-import UserManagement from './components/UserManagement';
-import UserProfile from './components/UserProfile';
-import CsvExcelUpload from './components/CsvExcelUpload';
-import Layout from './components/Layout';
-import NewPassword from './components/NewPassword';
-import TotalUsersInfo from './components/TotalUsersIfo';
-import ProtectedRoutes from './components/ProtectedRoutes';
-import { useState } from 'react';
-import { UserContext } from './utils/context';
-import { ToastContainer } from 'react-toastify';
+import { HashRouter as Route, Routes } from 'react-router-dom'
+import './App.css'
+import Login from './components/Login'
+import ForgetPassword from './components/ForgetPassword'
+import Dashboard from './components/Dashboard'
+import UserManagement from './components/UserManagement'
+import UserProfile from './components/UserProfile'
+import CsvExcelUpload from './components/CsvExcelUpload'
+import Layout from './components/Layout'
+import NewPassword from './components/NewPassword'
+import TotalUsersInfo from './components/TotalUsersIfo'
+import ProtectedRoutes from './components/ProtectedRoutes'
+import { useState } from 'react'
+import { UserContext } from './utils/context'
+import { ToastContainer, toast } from 'react-toastify';
 
 const App = () => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState(null)
 
   return (
     <>
-      <ToastContainer />
-      <UserContext.Provider value={{ data, setData }}>
-        <Router> {/* Wrap everything inside HashRouter */}
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/forgetpassword" element={<ForgetPassword />} />
-            <Route path="/newpassword" element={<NewPassword />} />
-
-            <Route element={<ProtectedRoutes />}>
-              <Route element={<Layout />}>
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/usermanagement" element={<UserManagement />} />
-                <Route path="/totalusersinfo" element={<TotalUsersInfo />} />
-                <Route path="/userprofile" element={<UserProfile />} />
-                <Route path="/csvexcelupload" element={<CsvExcelUpload />} />
-              </Route>
+    <ToastContainer />
+    <UserContext.Provider value={{ data, setData }}>
+      <Routes>
+          <Route path="/" element= {<Login />}/>
+          <Route path="/forgetpassword" element= {<ForgetPassword />}/>
+          <Route path="/newpassword" element= {<NewPassword />}/>
+          
+          <Route element={<ProtectedRoutes />}>
+            <Route element={<Layout/>}>
+            <Route path="/dashboard" element= {<Dashboard />}/>
+            <Route path="/usermanagement" element= {<UserManagement />}/>
+            <Route path="/totalusersinfo" element= {<TotalUsersInfo />}/>
+            <Route path="/userprofile" element= {<UserProfile />}/>
+            <Route path="/csvexcelupload" element= {<CsvExcelUpload />}/>
             </Route>
-          </Routes>
-        </Router>
+          </Route>
+        </Routes>
       </UserContext.Provider>
+       
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
